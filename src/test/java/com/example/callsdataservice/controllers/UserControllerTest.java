af764be43d19e399cc95222d8db366d10f07bc45
Lisa-1919 <lizasimerova438@gmail.com>
2024-02-25 16:34:53 +0300

    app run tests don't

--- src/test/java/com/example/callsdataservice/controllers/UserControllerTest.java
+++ src/test/java/com/example/callsdataservice/controllers/UserControllerTest.java
@@ -1,22 +1,11 @@
 package com.example.callsdataservice.controllers;
 
-import com.example.callsdataservice.models.User;
 import com.example.callsdataservice.security.services.UserDetailsServiceImpl;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(UserController.class)
 class UserControllerTest {
