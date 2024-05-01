8d0eed74653730db85226eff4f08fc6105f1ff8b
Lisa-1919 <lizasimerova438@gmail.com>
2024-05-01 17:04:24 +0300

    commit

--- src/main/java/com/example/callsdataservice/models/User.java
+++ src/main/java/com/example/callsdataservice/models/User.java
@@ -1,13 +1,22 @@
 package com.example.callsdataservice.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "users")
 @Entity
 public class User {
+    @Getter
     @Id@GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     @Column(name="username")
@@ -19,86 +28,18 @@ public class User {
     @Column(name = "language")
     private String language;
 
+    @Column(name = "image_name")
+    private String imageName;
+
     @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name="user_role",
             joinColumns = @JoinColumn(name="user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roleSet = new HashSet<>();
 
-    public User(){}
-    public User(Long id, String username, String email, String password) {
-        this.id = id;
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
-
-    public User(Long id, String username, String email, String password, String accountImgUrl, String language) {
-        this.id = id;
-        this.username = username;
-        this.email = email;
-        this.password = password;
-        this.language = language;
-    }
-
     public User(String username, String email, String password) {
         this.username = username;
         this.email = email;
         this.password = password;
     }
-
-    public User(String username) {
-        this.username = username;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getLanguage() {
-        return language;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
-    }
-
-    public Set<Role> getRoleSet() {
-        return roleSet;
-    }
-
-
-    public void setRoleSet(Set<Role> roleSet) {
-        this.roleSet = roleSet;
-    }
-
-
 }
